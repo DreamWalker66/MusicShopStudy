262582414a1605c369870dd6438964c9fef0d86d
dreamwalker <example.com>
2025-04-12 21:45:01 +0600

    Fixtures and goods

--- musicshop/main/views.py
+++ musicshop/main/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 # Create your views here.
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Главная',
         'content': 'Магазин пластинок',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
