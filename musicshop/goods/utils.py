93066140c435b182f82a90c479630790b963235e
dreamwalker <example.com>
2025-04-13 19:47:58 +0600

    Full text search done

--- musicshop/goods/utils.py
+++ musicshop/goods/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 
 from goods.models import Products
 
@@ -11,10 +11,28 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
     
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
 
+    result = result.annotate (
+        headline = SearchHeadline (
+            "name",
+            query,
+            start_sel = '<span style="background-color: lightblue;">',
+            stop_sel = "</span>",
+        )
+    )
 
-    
+    result = result.annotate (
+        bodyline = SearchHeadline (
+            "description",
+            query,
+            start_sel = '<span style="background-color: lightblue;">',
+            stop_sel = "</span>",
+        )
+    )
+
+    return result
+
     # keywords = [word for word in query.split() if len(word) > 1]
 
     # q_objects = Q()
