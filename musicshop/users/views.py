e203594913077a8bca48997d9fa49da1680c2c25
dreamwalker <example.com>
2025-04-16 15:16:42 +0600

    Registration form and controller are ready

--- musicshop/users/views.py
+++ musicshop/users/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
+# Логин
 def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
@@ -26,17 +27,36 @@ def login(request):
     }
     return render(request, 'users/login.html', context)
 
+# Регистрация
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Регистрация'
+        'title': 'Регистрация',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
+# Профиль
 def profile(request):
+
+
+
     context = {
-        'title': 'Кабинет'
+        'title': 'Кабинет',
     }
     return render(request, 'users/profile.html', context)
 
+# Выход из аккаунта
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
