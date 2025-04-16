33e9972fe094c37cfa1265c8270f355bf9135038
dreamwalker <example.com>
2025-04-16 19:43:30 +0600

    All cart templates were included

---
+++ musicshop/carts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# Добавление в корзину
+def cart_add(request, product_id):
+    ...
+
+# Изменение в корзине
+def cart_change(request, product_id):
+    ...
+
+# Удаление из корзины
+def cart_remove(request, product_id):
+    ...
